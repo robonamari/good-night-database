98286062127d86e75f500bc351fd089ef3f2d368
robonamari <ROBONAMARI@GMAIL.COM>
2024-08-06 04:37:22 +0330

    add

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 import random
 
-print(random.choice(requests.get('https://raw.githubusercontent.com/robonamari/Good_Night-database/main/database.json').json()['links']))
+print(random.choice(requests.get("https://raw.githubusercontent.com/robonamari/Good_Night-database/main/database.json").json()["messages"]))
