76ff86eecc43729c11b420f53684cd3dc770522d
robonamari <ROBONAMARI@GMAIL.COM>
2024-08-09 13:34:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import requests
 import random
 
 print(random.choice(requests.get("https://good-night-database.robonamari.ir/database.json").json()["messages"]))
+# or
+print(random.choice(requests.get("https://good-night-database.robonamari.ir/database.json").json()["gifs"]))
