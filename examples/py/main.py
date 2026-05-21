2f2cc042f8266a1e939976c64c27786b566f0bb0
RoboNamari <107060501+robonamari@users.noreply.github.com>
2026-05-21 16:50:03 +0330

    Refactor print statements for better readability

--- examples/py/main.py
+++ examples/py/main.py
@@ -3,19 +3,19 @@ import random
 import requests  # pip install requests
 
 print(
-random.choice(
-    requests.get("https://good-night-database.robonamari.com/database.json").json()[
-        "texts"
-    ]
-)
+    random.choice(
+        requests.get("https://good-night-database.robonamari.com/database.json").json()[
+            "texts"
+        ]
+    )
 )
 
 # or
 
 print(
-random.choice(
-    requests.get("https://good-night-database.robonamari.com/database.json").json()[
-        "gifs"
-    ]
-)
+    random.choice(
+        requests.get("https://good-night-database.robonamari.com/database.json").json()[
+            "gifs"
+        ]
+    )
 )
