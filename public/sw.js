3eb77e78c2efb12d9eac49404a4730501cdf75b6
RoboNamari <107060501+robonamari@users.noreply.github.com>
2026-06-11 23:10:05 +0330

    Refactor service worker caching routes and strategies for improved organization

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,15 @@
-const CACHE = "app-shell-v1";
+const APP_SHELL_CACHE = "app-shell-v1";
+const DATA_CACHE = "app-data-v1";
+const GIF_CACHE = "gif-cache-v1";
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 workbox.routing.registerRoute(
   ({
     url
-  }) =>
-    url.pathname === "/app/" ||
-    url.pathname === "/app/gifs" ||
-    url.pathname === "/app/texts",
+  }) => url.pathname.startsWith("/app/"),
   new workbox.strategies.CacheFirst({
-    cacheName: CACHE
+    cacheName: APP_SHELL_CACHE,
   })
 );
 
@@ -18,5 +17,22 @@ workbox.routing.registerRoute(
   ({
     url
   }) => url.pathname.includes("database.json"),
-  new workbox.strategies.NetworkOnly()
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: DATA_CACHE
+  })
+);
+
+workbox.routing.registerRoute(
+  ({
+    url
+  }) => url.pathname.endsWith(".gif"),
+  new workbox.strategies.CacheFirst({
+    cacheName: GIF_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        maxEntries: 100,
+      }),
+    ],
+  })
 );
